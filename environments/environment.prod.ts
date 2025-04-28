f313d87913cad271812b25658cdfbe362e537a80
herbie30 <63430818+herbie30@users.noreply.github.com>
2025-04-28 01:10:08 +0100

    Delete environments directory

--- environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-// src/environments/environment.prod.ts
-export const environment = {
-  production: true,
-  // add any prod‐only overrides here
-};
--- environments/environment.ts
+++
@@ -1,5 +0,0 @@
-// src/environments/environment.ts
-export const environment = {
-  production: false,
-  // add any other dev‐only settings here
-};
